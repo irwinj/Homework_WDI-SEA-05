0ac9c6d92c26397815e29b9bb4e284bd49828784
irwinj <jirwin2@gmail.com>
2015-11-18 09:12:53 -0800

    Homework week 2 day 2

---
+++ irwinj/week02/day02/script.js
@@ -0,0 +1,13 @@
+$('form').submit(function () {
+    if ($('input').val() !== '') {
+        var input_value = $('input').val();
+        $('ul').append('<li>' + input_value + '<a href="">x</a></li>');
+    };
+    $('input').val('');
+    return false;
+});
+
+$(document).on('click', 'a', function (e) {
+    e.preventDefault();
+    $(this).parent().remove();
+});
