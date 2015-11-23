daca44c0b3a497e12a345fb0244a9e47dbefed3b
irwinj <jirwin2@gmail.com>
2015-11-23 15:51:52 -0800

    more updates to functionality

--- irwinj/week03/project/script.js
+++ irwinj/week03/project/script.js
@@ -1,41 +1,154 @@
-div with a box where you enter number of players (1-6), i
-var numPlay = i
-//number of players - sets $json.(playVar(army)) according to number of players
+// docready
 
-var playVar
-//numPlay modified to current player turn
 
-var attackedCountry =
-	($json.countryData(attack))
 
-function allCount(){
-	each.countries()
-} //checks countries for a value
+var countryData = $.getJSON('countries.json');
+var players = {}; //used to change players, check if <=6
+var playerTurn = 1;
 
-function allPlay(){
-} //checks all countries for owner, adds them up 1+1, see if anyone = total countries
+setPlayers();  //not sure what this does
 
-function allReserve(){	
-} //returns number of armies all player has, or sets it at the start of game
+$('.h1').html(countryData.currentCountry.army) //this is how we'll change CSS to reflect json
 
-function country(){
-}	// returns a value inside of country array
+playerTurn++;
+var newPlayer = if (('player' + playerTurn <= 6, newPlayer)
+				else newPlayer = 1;
+//playerturn change should summon initPop also needs 
+players[newPlayer];
 
-function changePlay(){
-	playVar - 1;
-		if playVar = 0, set playVar = numPlay
-} //changes player unless it's 0, then it becomes the number of players
+
+
+//set number of players (2-6), distributes armies to players which allows initpop to work
+function setPlayers() {
+    var playNum = prompt("Please select number of players", "2 - 6");
+	if (playNum === '2') {
+		for (var i = 1; i <= parseInt(playNum); i++) {
+			var playerName = 'player' + playNum;
+			players[playerName] = {};
+			players[playerName].reserve = 40;
+			$('#h2').html(players[playerTurn] + ", place an army on an open country!"));
+		}
+		initPop(playNum);
+	} else if (playNum === '3') {
+		for (var i = 1; i <= parseInt(playNum); i++) {
+			var playerName = 'player' + playNum;
+			players[playerName] = {};
+			players[playerName].reserve = 35;
+			$('#h2').html(players[playerTurn] + ", place an army on an open country!"));
+		}
+		initPop(playNum);
+	} else if (playNum === 4) {
+		for (var i = 1; i <= parseInt(playNum); i++) {
+			var playerName = 'player' + playNum;
+			players[playerName] = {};
+			players[playerName].reserve = 30;
+			$('#h2').html(players[playerTurn] + ", place an army on an open country!"));
+		}
+		initPop(playNum);
+	} else if (playNum === 5) {
+		for (var i = 1; i <= parseInt(playNum); i++) {
+			var playerName = 'player' + playNum;
+			players[playerName] = {};
+			players[playerName].reserve = 25;
+			$('#h2').html(players[playerTurn] + ", place an army on an open country!"));
+		}
+		initPop(playNum);
+	} else if (playNum === 6) {
+		for (var i = 1; i <= parseInt(playNum); i++) {
+			var playerName = 'player' + playNum;
+			players[playerName] = {};
+			players[playerName].reserve = 35;
+			$('#h2').html(players[playerTurn] + ", place an army on an open country!"));
+		}
+		initPop(playNum);
+	} else {
+		setPlayers();
+	}
+
+
+	});
+}
+
+function initPop(playNum){ //populate empty countries, declare winner
+	if ($each.countryData = 0) { //??any countries have 0 armies
+		if (players[newPlayer] > 0) { //current player has reserve
+			$('.country').click(function() { //add armies to open country
+				var countryName = $(this).attr('id');
+				if(countryData.countryName.army = 0) {
+					addArmy(countryName);
+					countryName.owner = players[playerTurn];
+					checkWinner(); //if anyone's array is 42, they win
+					playerTurn++;
+				} else {
+					$('#h2').html(players[playerTurn] + ", place an army on an open country!");
+				}
+			}
+		} else {
+			playerTurn++; //some player must have armies in reserve still
+		}; 
+	} else {
+		$('#h2').html(players[playerTurn] + ", place your remaining armies!"));
+		regPop()
+	}
+}
+
+function addArmy(countryName) {
+	countryName.army + 1;
+	players[playerTurn].reserve - 1;
+	playerTurn++;//if player doesn't match playerTurn, it becomes other player's turn
+}
+			
+function regPop() {	//all countries have >0 armies, moving on to regular population
+	if (players{}.sum>0); //someone still has armies in reserve
+		if (players[playerTurn].reserve > 0) {//current player has reserves
+		//can i set a separate click listener, or do i pass that from earlier?
+			$('.country').click(function() { //add armies to open country
+				var countryName = $(this).attr('id');
+				if(countryData.countryName.owner == players[PlayerTurn]) {
+					addArmy(countryName);
+					playerTurn++;
+					$('#h2').html(players[playerTurn] + ", place your remaining armies");
+				} else {
+					$('#h2').html(players[playerTurn] + ", please place armies in your country!");
+				}
+			}
+		else {
+			playerTurn++;
+		}
+	else {
+		$('#h2').html(players[playerTurn] + " choose country to attack from, or end your turn")
+		chooseAtt() //if all reserves are played, move on to attack
+	}
+}
+
+function WINNER() ////if anyone's array is 42, they win
+	array.countryName.owner= 42, win! //pseudocode
+
+var attackingCountry = countryData.attack.1
+// sets attacker country
+
+var attackedCountry = countryData.attack.2
+//sets defender country
 	
+// diceroll
 function diceRoll(playerRoll){
 	playerRoll.push(Math.floor(Math.random()*6)+1);
 }
 var attackerRoll = {};
-
+//pushes attack and defense roll to an array
 var defenseRoll = {};
 
 function diceRolls(armyCount){
-	armyCount.text - 1
-		if >=3, = diceRoll(attackerRoll) x 3;
+	$('.country').click(function() { //add armies to open country
+		var countryName = $(this).attr('id');
+		if (countryData.countryName.owner == players[PlayerTurn]) {
+			countryData.countryName.attack = 1
+		} 
+
+			var countryName = $(this).attr('id');
+				if(countryData.countryName.owner == players[PlayerTurn])
+	countryData.army - 1
+		if >=3, = diceRoll(attackerRoll) x 3; //how do i declare a roll three times
 		else = diceRoll(attackerRoll) x value //need to make this work for both
 }	//same for defense up to 2
 
@@ -49,6 +162,7 @@ function armyCount(x){
 }	//find country name json element in defenseState attack 2, return CSS text field
 //fix variables and this stuff tomorrow querySelector FILTER
 function armyChange(attdef){
+
 	if ((attackRoll[0] - defenseRoll[0]) > 0){
 		armyCount(1).text - 1, setCSSClassText armyCount
 	else(var x = armyCount(2).text - 1, setCSSClassText armyCount, setCSSClassText)
@@ -78,63 +192,10 @@ ENDTURN BUTTON
 			initPop
 			else alert (playVar + "You cannot end your turn while you have armies in reserve!"
 
-// swith(gamestate) {
-// 	case: "init"
-// 		initPop(e);
-// 		break;
-// 	case: "addArmy"
-// 		addArmy(e);
-// 		break;
-
-// }  no switch since I did a heuristic
-
-// Switch (gamestate)
-// //activated when a player clicks
-// 	initPop(playVar)
-// 		break
-// 	addArmy
-// 		break
-// 	playerTurn
-// 		break
-// 	cancelAttack
-// 		break
-// 	attackPlayer
-// 		break
 
 
-initPop //(SWITCH 1)
-	allCount(armies) = 0
-	else = AddArmy
-	//if any countries have 0 armies, continues
-	playVar(army) > 0
-		else changePlay, switch
-	//and player has more than 0 armies (or it loops back)
-	country(armies) = 0
-	//and the country has no armies (otherwise do nothing)
-		FUNCTION = then +1 playerarmy country(armies) && -1 playVar(army)
-		//adds 1 to country
-		allPlay(owner) = ##, WINNER!  //index of player.countries
-			else updates scoreboard //(if I have time)
-		//totals player countries, checks if any player owns the total number of countries, if so they win!
-		changePlay
-		if allCount = 0
-				if playVar(army) > 0, ("Please place an army on an open country, " + playVar)
-				else switch
-				//there is an open country, so cycling through until "playVar(army)" > 0
-		else allCount > 0
-				switch
-				//all countries have an army, so moving on to populate countries
 
-addArmy //(SWITCH 2)
-	allReserve > 0 //checks if all players have any armies in reserve
-		playVar(army) > 0
-			else changePlay, addArmy
-		//player has armies, so we'll add armies to countries (or it changes player and loops back)
-		country(owner) = playVar
-		//and it is the player's country (otherwise do nothing)
-			then +1 country(army), -1 playVar(army), changePlay
-				if playVar(army) > 0, ("Please place an army on your country, " + playVar)
-				else switch, 
+
 
 playerTurn
 	allCount(attack) = 0 //player hasn't declared a country to attack from
